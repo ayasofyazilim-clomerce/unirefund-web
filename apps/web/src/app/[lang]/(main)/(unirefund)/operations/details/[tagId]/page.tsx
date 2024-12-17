62c88604ce55946e040ac9efea187f8a44581ca3
Eren <ahmeterenodaci@gmail.com>
2024-12-17 16:54:49 +0300

    refactor(tag/detail): divide to components

--- apps/web/src/app/[lang]/(main)/(unirefund)/operations/details/[tagId]/page.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/operations/details/[tagId]/page.tsx
@@ -4,8 +4,9 @@ import { notFound } from "next/navigation";
 import { getTagById } from "src/actions/unirefund/TagService/actions";
 import { getResourceData } from "src/language-data/unirefund/TagService";
 import MerchantDetails from "./_components/merchant-details";
+import TagStatuses from "./_components/tag-statuses";
 import TagSummary from "./_components/tag-summary";
-import Totals from "./_components/totals";
+import TagTabs from "./_components/tag-tabs";
 import TravellerDetails from "./_components/traveller-details";
 
 export default async function Page({
@@ -20,14 +21,13 @@ export default async function Page({
 
   const tagDetail = response.data;
   return (
-    <div className="flex flex-row gap-3">
-      <Totals languageData={languageData} tagDetail={tagDetail} />
-      <div className="flex w-2/3 flex-col">
+    <div className="gap-3">
+      <div className="grid grid-cols-2 gap-3">
         <TagSummary languageData={languageData} tagDetail={tagDetail} />
-      </div>
-      <div className="flex w-1/3 flex-col">
         <MerchantDetails languageData={languageData} tagDetail={tagDetail} />
+        <TagStatuses languageData={languageData} tagDetail={tagDetail} />
         <TravellerDetails languageData={languageData} tagDetail={tagDetail} />
+        <TagTabs languageData={languageData} tagDetail={tagDetail} />
       </div>
     </div>
   );
