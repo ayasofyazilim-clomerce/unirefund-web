c7745a97d811ca40f5cbc4fe3ee8ba872db3819d
yusualhashash <yusufalhashash2018@gmail.com>
2024-12-18 19:42:50 +0300

    feat: add Tax Free Tag table

---
+++ apps/web/src/app/[lang]/(main)/(unirefund)/finance/vat-statements/[vatStatementId]/_components/tax-free-tag/table.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import type { UniRefund_FinanceService_VATStatementHeaders_VATStatementHeaderDetailDto } from "@ayasofyazilim/saas/FinanceService";
+import TanstackTable from "@repo/ayasofyazilim-ui/molecules/tanstack-table";
+import { useParams } from "next/navigation";
+import type { FinanceServiceResource } from "src/language-data/unirefund/FinanceService";
+import { useGrantedPolicies } from "src/providers/granted-policies";
+import { tableData } from "./tax-free-tag-table-data";
+
+function TaxFreeTagTable({
+  languageData,
+  taxFreeTagsData,
+}: {
+  taxFreeTagsData: UniRefund_FinanceService_VATStatementHeaders_VATStatementHeaderDetailDto;
+  languageData: FinanceServiceResource;
+}) {
+  const { grantedPolicies } = useGrantedPolicies();
+  const { lang } = useParams<{ lang: string }>();
+
+  const columns = tableData.TaxFreeTag.columns(
+    lang,
+    languageData,
+    grantedPolicies,
+  );
+  const table = tableData.TaxFreeTag.table();
+
+  return (
+    <TanstackTable
+      {...table}
+      columns={columns}
+      data={taxFreeTagsData.vatStatementTagDetails || []}
+      rowCount={taxFreeTagsData.vatStatementTagDetails?.length || 0}
+    />
+  );
+}
+
+export default TaxFreeTagTable;
