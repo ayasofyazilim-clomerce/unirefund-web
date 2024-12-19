121e8347cf95a62df2f6978ee7768fae1649388d
Eren <ahmeterenodaci@gmail.com>
2024-12-19 17:30:31 +0300

    chore(tax-free-tags): add refund

---
+++ apps/web/src/app/[lang]/(main)/(unirefund)/operations/tax-free-tags/refund-form.tsx
@@ -0,0 +1,202 @@
+"use client";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import type { UniRefund_CRMService_Merchants_RefundPointProfileDto } from "@ayasofyazilim/saas/CRMService";
+import type {
+  UniRefund_TagService_Tags_RefundType,
+  UniRefund_TagService_Tags_TagListItemDto,
+} from "@ayasofyazilim/saas/TagService";
+import { AlertCircle, Banknote, CreditCard } from "lucide-react";
+import { useState, useTransition } from "react";
+import { handlePostResponse } from "src/actions/core/api-utils-client";
+import { postRefundApi } from "src/actions/unirefund/RefundService/post-actions";
+
+export function RefundForm({
+  refundPoints,
+  selectedRows,
+}: {
+  selectedRows: UniRefund_TagService_Tags_TagListItemDto[];
+  refundPoints: UniRefund_CRMService_Merchants_RefundPointProfileDto[];
+}) {
+  const [refundMethod, setRefundMethod] = useState<string>("Cash");
+  const [selectedRefundPointId, setSelectedRefundPointId] = useState("");
+  const [isPending, startTransition] = useTransition();
+
+  const canRefundable =
+    selectedRows
+      .map((i) => i.travellerDocumentNumber)
+      .filter((i) => i !== selectedRows[0].travellerDocumentNumber).length ===
+    0;
+
+  if (selectedRows.length === 0) return null;
+
+  function onSubmit() {
+    startTransition(() => {
+      void postRefundApi({
+        requestBody: {
+          refundType: refundMethod as UniRefund_TagService_Tags_RefundType,
+          refundPointId: selectedRefundPointId,
+          tagIds: selectedRows.map((tag) => tag.id || ""),
+        },
+      }).then((response) => {
+        handlePostResponse(response);
+      });
+    });
+  }
+  return (
+    <>
+      <div className="flex flex-col gap-2 rounded-lg bg-gray-100 p-6">
+        {selectedRows.length} transaction selected
+      </div>
+      {!canRefundable && (
+        <Alert className="my-3" variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>
+            You can not have transactions from different travellers in the same
+            refund.
+          </AlertDescription>
+        </Alert>
+      )}
+      <div className="my-3 flex flex-col space-y-1.5">
+        <div className="font-semibold leading-none">Payment Method</div>
+        <div className="text-muted-foreground text-sm">
+          Select a payment method.
+        </div>
+      </div>
+      <div className="my-3 grid gap-6">
+        <RadioGroup
+          className="grid grid-cols-2 gap-4"
+          defaultValue={refundMethod}
+          onValueChange={setRefundMethod}
+        >
+          <div>
+            <RadioGroupItem
+              aria-label="Card"
+              className="peer sr-only"
+              id="card"
+              value="CreditCard"
+            />
+            <Label
+              className="border-muted hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary flex flex-col items-center justify-between rounded-md border-2 bg-transparent p-4"
+              htmlFor="card"
+            >
+              <CreditCard className="mb-3 h-6 w-6" />
+              Card
+            </Label>
+          </div>
+          <div>
+            <RadioGroupItem
+              aria-label="Cash"
+              className="peer sr-only"
+              id="cash"
+              value="Cash"
+            />
+            <Label
+              className="border-muted hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary flex flex-col items-center justify-between rounded-md border-2 bg-transparent p-4"
+              htmlFor="cash"
+            >
+              <Banknote className="mb-3 h-6 w-6" />
+              Cash
+            </Label>
+          </div>
+        </RadioGroup>
+
+        {refundMethod === "CreditCard" && (
+          <>
+            <div className="grid gap-2">
+              <Label htmlFor="number">Card number</Label>
+              <Input id="number" placeholder="" />
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="month">Expires</Label>
+                <Select>
+                  <SelectTrigger aria-label="Month" id="month">
+                    <SelectValue placeholder="Month" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="1">January</SelectItem>
+                    <SelectItem value="2">February</SelectItem>
+                    <SelectItem value="3">March</SelectItem>
+                    <SelectItem value="4">April</SelectItem>
+                    <SelectItem value="5">May</SelectItem>
+                    <SelectItem value="6">June</SelectItem>
+                    <SelectItem value="7">July</SelectItem>
+                    <SelectItem value="8">August</SelectItem>
+                    <SelectItem value="9">September</SelectItem>
+                    <SelectItem value="10">October</SelectItem>
+                    <SelectItem value="11">November</SelectItem>
+                    <SelectItem value="12">December</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="year">Year</Label>
+                <Select>
+                  <SelectTrigger aria-label="Year" id="year">
+                    <SelectValue placeholder="Year" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {Array.from({ length: 10 }, (_, i) => (
+                      <SelectItem
+                        key={i}
+                        value={`${new Date().getFullYear() + i}`}
+                      >
+                        {new Date().getFullYear() + i}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="cvc">CVC</Label>
+                <Input id="cvc" placeholder="CVC" />
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+      <Select
+        onValueChange={setSelectedRefundPointId}
+        value={selectedRefundPointId}
+      >
+        <SelectTrigger className="mb-3 w-full">
+          <SelectValue placeholder="Select a refund point" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Refund Points</SelectLabel>
+            {refundPoints.map((refundPoint) => (
+              <SelectItem key={refundPoint.id} value={refundPoint.id || ""}>
+                {refundPoint.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <div className="flex items-center">
+        <Button
+          className="w-full"
+          disabled={!canRefundable || refundMethod !== "Cash" || isPending}
+          onClick={onSubmit}
+        >
+          Continue
+        </Button>
+      </div>
+    </>
+  );
+}
