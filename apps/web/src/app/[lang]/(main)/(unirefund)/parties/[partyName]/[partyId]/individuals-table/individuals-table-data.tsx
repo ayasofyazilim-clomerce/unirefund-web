d0d9726f7b99612fb086d7708b57fa4e828a2e6e
Eren <ahmeterenodaci@gmail.com>
2024-12-20 15:06:44 +0300

    chore(individuals): ability to add individual to party

---
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/[partyName]/[partyId]/individuals-table/individuals-table-data.tsx
@@ -0,0 +1,67 @@
+import type { UniRefund_CRMService_AffiliationTypes_AffiliationTypeDto } from "@ayasofyazilim/saas/CRMService";
+import { $UniRefund_CRMService_AffiliationTypes_AffiliationTypeDto } from "@ayasofyazilim/saas/CRMService";
+import type { TanstackTableCreationProps } from "@repo/ayasofyazilim-ui/molecules/tanstack-table/types";
+import { tanstackTableCreateColumnsByRowData } from "@repo/ayasofyazilim-ui/molecules/tanstack-table/utils";
+import type {
+  FieldConfigType,
+  ZodObjectOrWrapped,
+} from "@repo/ayasofyazilim-ui/organisms/auto-form";
+import { PlusCircle } from "lucide-react";
+import type { CRMServiceServiceResource } from "src/language-data/unirefund/CRMService";
+import type { AutoFormValues } from "./table";
+
+type IndividualsTable =
+  TanstackTableCreationProps<UniRefund_CRMService_AffiliationTypes_AffiliationTypeDto>;
+
+export function individualsColumns(
+  languageData: CRMServiceServiceResource,
+  locale: string,
+) {
+  return tanstackTableCreateColumnsByRowData<UniRefund_CRMService_AffiliationTypes_AffiliationTypeDto>(
+    {
+      languageData,
+      rows: $UniRefund_CRMService_AffiliationTypes_AffiliationTypeDto.properties,
+      config: {
+        locale,
+      },
+    },
+  );
+}
+
+function individualsTable(
+  languageData: CRMServiceServiceResource,
+  addAffilationsFormSchema: ZodObjectOrWrapped,
+  handleSubmit: (formData: AutoFormValues) => Promise<void>,
+  fieldConfig: FieldConfigType,
+): IndividualsTable {
+  const table: IndividualsTable = {
+    fillerColumn: "name",
+    columnOrder: ["name"],
+    columnVisibility: {
+      type: "show",
+      columns: ["name", "creationTime"],
+    },
+    tableActions: [
+      {
+        type: "autoform-dialog",
+        actionLocation: "table",
+        cta: languageData["Individuals.New"],
+        title: languageData["Individuals.New"],
+        schema: addAffilationsFormSchema,
+        fieldConfig,
+        icon: PlusCircle,
+        onSubmit(values) {
+          void handleSubmit(values as AutoFormValues);
+        },
+        submitText: languageData.Save,
+      },
+    ],
+  };
+  return table;
+}
+export const tableData = {
+  individuals: {
+    columns: individualsColumns,
+    table: individualsTable,
+  },
+};
