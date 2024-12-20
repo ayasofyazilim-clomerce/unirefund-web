3bd70f3b541403b14493e5b3424ea60197d96c8c
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-12-20 11:05:11 +0300

    feat: check tax-office before create party

--- apps/web/src/app/[lang]/(main)/(unirefund)/parties/[partyName]/new/page.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/[partyName]/new/page.tsx
@@ -1,5 +1,9 @@
 "use server";
 
+import { FileText } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { FormReadyComponent } from "@repo/ui/form-ready";
 import { getTaxOfficesApi } from "src/actions/unirefund/CrmService/actions";
 import { getResourceData } from "src/language-data/unirefund/CRMService";
 import { getCountriesApi } from "../../../../../../../actions/unirefund/LocationService/actions";
@@ -10,10 +14,11 @@ export default async function Page({
   params,
 }: {
   params: {
-    partyName: Exclude<PartyNameType, "individuals">;
+    partyName: PartyNameType;
     lang: string;
   };
 }) {
+  const { partyName } = params;
   const { languageData } = await getResourceData(params.lang);
 
   const countries = await getCountriesApi();
@@ -25,11 +30,31 @@ export default async function Page({
     (taxOffices.type === "success" && taxOffices.data.items) || [];
 
   return (
-    <PageClientSide
-      countryList={countryList}
-      languageData={languageData}
-      partyName={params.partyName}
-      taxOfficeList={taxOfficeList}
-    />
+    <FormReadyComponent
+      active={
+        taxOfficeList.length === 0 &&
+        partyName !== "tax-offices" &&
+        (partyName === "refund-points" || partyName === "merchants")
+      }
+      content={{
+        icon: <FileText className="size-20 text-gray-400" />,
+        title: languageData["Missing.TaxOffices.Title"],
+        message: languageData["Missing.TaxOffices.Message"],
+        action: (
+          <Button asChild className="text-blue-500" variant="link">
+            <Link href="../tax-offices/new">
+              {languageData["TaxOffices.New"]}
+            </Link>
+          </Button>
+        ),
+      }}
+    >
+      <PageClientSide
+        countryList={countryList}
+        languageData={languageData}
+        partyName={params.partyName}
+        taxOfficeList={taxOfficeList}
+      />
+    </FormReadyComponent>
   );
 }
