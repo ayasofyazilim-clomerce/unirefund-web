b559340c3a92a1304f1ad0229bd671773207bca5
yusualhashash <yusufalhashash2018@gmail.com>
2024-12-25 09:30:43 +0300

    refactor(Product groups): use tanstack table

---
+++ apps/web/src/app/[lang]/(main)/(unirefund)/settings/product/product-groups/page.tsx
@@ -0,0 +1,32 @@
+"use server";
+
+import type { GetApiSettingServiceProductGroupData } from "@ayasofyazilim/saas/SettingService";
+import { getproductGroupsApi } from "src/actions/unirefund/SettingService/actions";
+import { getResourceData } from "src/language-data/unirefund/SettingService";
+import { isUnauthorized } from "src/utils/page-policy/page-policy";
+import { isErrorOnRequest } from "src/utils/page-policy/utils";
+import ProductGroupsTable from "./_components/table";
+
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { lang: string };
+  searchParams: GetApiSettingServiceProductGroupData;
+}) {
+  const { lang } = params;
+  await isUnauthorized({
+    requiredPolicies: ["SettingService.ProductGroups"],
+    lang,
+  });
+  const productGroupsResponse = await getproductGroupsApi(searchParams);
+  if (isErrorOnRequest(productGroupsResponse, lang)) return;
+  const { languageData } = await getResourceData(lang);
+
+  return (
+    <ProductGroupsTable
+      languageData={languageData}
+      response={productGroupsResponse.data}
+    />
+  );
+}
