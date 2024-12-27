bba2c82a81254a1fa75c5ce9e2b014ebbe89d54c
Eren <ahmeterenodaci@gmail.com>
2024-12-27 11:12:58 +0300

    feat: replace Popover with Combobox for refund point selection in traveller document form

--- apps/web/src/app/[lang]/(main)/(unirefund)/operations/refund/_components/traveller-document-form.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/operations/refund/_components/traveller-document-form.tsx
@@ -1,24 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { cn } from "@/lib/utils";
 import type { UniRefund_CRMService_Merchants_RefundPointProfileDto } from "@ayasofyazilim/saas/CRMService";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Combobox } from "@repo/ayasofyazilim-ui/molecules/combobox";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState, useTransition } from "react";
 import type { TagServiceResource } from "src/language-data/unirefund/TagService";
@@ -42,13 +28,15 @@ export default function TravellerDocumentForm({
 
   const [travellerDocumentNoInput, setTravellerDocumentNoInput] =
     useState(travellerDocumentNo);
-  const [refundPointIdInput, setRefundPointIdInput] = useState(refundPointId);
+  const [refundPointIdInput, setRefundPointIdInput] = useState<
+    UniRefund_CRMService_Merchants_RefundPointProfileDto | null | undefined
+  >(accessibleRefundPoints.find((i) => i.id === refundPointId) || null);
   const [isPending, startTransition] = useTransition();
 
   function searchForTraveller() {
     startTransition(() => {
       router.replace(
-        `${pathName}?travellerDocumentNumber=${travellerDocumentNoInput}&refundPointId=${refundPointIdInput}`,
+        `${pathName}?travellerDocumentNumber=${travellerDocumentNoInput}&refundPointId=${refundPointIdInput?.id}`,
       );
     });
   }
@@ -76,67 +64,22 @@ export default function TravellerDocumentForm({
       </div>
       <div className="grid max-w-lg items-center gap-1.5">
         <Label htmlFor="refund-point">{languageData.RefundPoint}</Label>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              className="w-[300px] justify-between"
-              role="combobox"
-              variant="outline"
-            >
-              {refundPointIdInput
-                ? accessibleRefundPoints.find(
-                    (refundPoint) => refundPoint.id === refundPointIdInput,
-                  )?.name
-                : languageData["Select.EmptyValue"]}
-              <ChevronsUpDown className="opacity-50" />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[200px] p-0">
-            <Command>
-              <CommandInput
-                className="h-9"
-                placeholder={languageData["Select.Placeholder"]}
-              />
-              <CommandList>
-                <CommandEmpty>{languageData["Select.EmptyValue"]}</CommandEmpty>
-                <CommandGroup>
-                  {accessibleRefundPoints.map((refundPoint) => (
-                    <CommandItem
-                      key={refundPoint.id}
-                      onSelect={(currentValue) => {
-                        setRefundPointIdInput(
-                          currentValue === refundPointIdInput
-                            ? ""
-                            : currentValue,
-                        );
-                      }}
-                      value={refundPoint.id || ""}
-                    >
-                      {refundPoint.name}
-                      <Check
-                        className={cn(
-                          "ml-auto",
-                          refundPointIdInput === refundPoint.id
-                            ? "opacity-100"
-                            : "opacity-0",
-                        )}
-                      />
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
+        <Combobox<UniRefund_CRMService_Merchants_RefundPointProfileDto>
+          list={accessibleRefundPoints}
+          onValueChange={setRefundPointIdInput}
+          selectIdentifier="id"
+          selectLabel="name"
+          value={refundPointIdInput}
+        />
       </div>
       <Button
         className="w-24"
         disabled={
           isPending ||
           !travellerDocumentNoInput.length ||
-          !refundPointIdInput.length ||
+          !refundPointIdInput?.id ||
           (travellerDocumentNo === travellerDocumentNoInput &&
-            refundPointId === refundPointIdInput)
+            refundPointId === refundPointIdInput.id)
         }
         onClick={searchForTraveller}
         type="submit"
