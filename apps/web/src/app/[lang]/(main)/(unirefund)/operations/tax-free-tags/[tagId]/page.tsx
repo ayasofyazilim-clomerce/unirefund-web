b394b5d5898a78ed90728744671bc6f7e0637c40
Eren <ahmeterenodaci@gmail.com>
2025-01-03 14:17:32 +0300

    fix(tag): add accessible refund points to TagActions component

--- apps/web/src/app/[lang]/(main)/(unirefund)/operations/tax-free-tags/[tagId]/page.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/operations/tax-free-tags/[tagId]/page.tsx
@@ -4,6 +4,7 @@ import { getTagByIdApi } from "src/actions/unirefund/TagService/actions";
 import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
 import { getResourceData } from "src/language-data/unirefund/TagService";
 import { isErrorOnRequest } from "src/utils/page-policy/utils";
+import { getAccessibleRefundPointsApi } from "src/actions/unirefund/CrmService/actions";
 import MerchantDetails from "./_components/merchant-details";
 import TagStatuses from "./_components/tag-statuses";
 import TagSummary from "./_components/tag-summary";
@@ -17,7 +18,6 @@ export default async function Page({
   params: { tagId: string; lang: string };
 }) {
   const { tagId, lang } = params;
-
   const { languageData } = await getResourceData(lang);
 
   const tagDetailResponse = await getTagByIdApi({ id: tagId });
@@ -30,10 +30,22 @@ export default async function Page({
     );
   }
 
+  const accessibleRefundPointsResponse = await getAccessibleRefundPointsApi();
+  const accessibleRefundPoints =
+    (accessibleRefundPointsResponse.type === "success" &&
+      accessibleRefundPointsResponse.data.items) ||
+    [];
+
   const tagDetail = tagDetailResponse.data;
   return (
     <div className="mb-2 grid h-full grid-cols-2 gap-3 overflow-auto">
-      <TagActions languageData={languageData} tagDetail={tagDetail} />
+      {accessibleRefundPoints.length > 0 && (
+        <TagActions
+          accessibleRefundPoints={accessibleRefundPoints}
+          languageData={languageData}
+          tagDetail={tagDetail}
+        />
+      )}
       <TagSummary languageData={languageData} tagDetail={tagDetail} />
       <MerchantDetails languageData={languageData} tagDetail={tagDetail} />
       <TagStatuses languageData={languageData} tagDetail={tagDetail} />
