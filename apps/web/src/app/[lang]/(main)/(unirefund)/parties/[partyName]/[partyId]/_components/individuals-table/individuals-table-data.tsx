67c0caf3d9b1a7a04b8b8a96ff9b2501bc2c9ccf
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-15 15:26:49 +0300

    feat: add action list to individual page

--- apps/web/src/app/[lang]/(main)/(unirefund)/parties/[partyName]/[partyId]/_components/individuals-table/individuals-table-data.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/[partyName]/[partyId]/_components/individuals-table/individuals-table-data.tsx
@@ -1,17 +1,143 @@
 import type { UniRefund_CRMService_AffiliationTypes_AffiliationTypeDetailDto } from "@ayasofyazilim/saas/CRMService";
 import { $UniRefund_CRMService_AffiliationTypes_AffiliationTypeDetailDto } from "@ayasofyazilim/saas/CRMService";
-import type { TanstackTableCreationProps } from "@repo/ayasofyazilim-ui/molecules/tanstack-table/types";
+import type {
+  TanstackTableCreationProps,
+  TanstackTableRowActionsType,
+} from "@repo/ayasofyazilim-ui/molecules/tanstack-table/types";
 import { tanstackTableCreateColumnsByRowData } from "@repo/ayasofyazilim-ui/molecules/tanstack-table/utils";
 import type {
   FieldConfigType,
   ZodObjectOrWrapped,
 } from "@repo/ayasofyazilim-ui/organisms/auto-form";
-import { CheckCircle, PlusCircle, XCircle } from "lucide-react";
+import { SchemaForm } from "@repo/ayasofyazilim-ui/organisms/schema-form";
+import { Eye, KeyIcon, PlusCircle, User2 } from "lucide-react";
+import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { postSendPasswordResetCodeApi } from "src/actions/core/AccountService/post-actions";
+import {
+  handlePostResponse,
+  handlePutResponse,
+} from "src/actions/core/api-utils-client";
+import {
+  putUsersByIdLockByLockoutEndApi,
+  putUsersByIdUnlockApi,
+} from "src/actions/core/IdentityService/put-actions";
+import { postAbpUserAccountByIndividualIdApi } from "src/actions/unirefund/CrmService/post-actions";
 import type { CRMServiceServiceResource } from "src/language-data/unirefund/CRMService";
 import type { AutoFormValues } from "./table";
 
 type IndividualsTable =
   TanstackTableCreationProps<UniRefund_CRMService_AffiliationTypes_AffiliationTypeDetailDto>;
+
+interface FormData {
+  lockoutEnd?: string;
+}
+
+function individualsRowActions(
+  languageData: CRMServiceServiceResource,
+  router: AppRouterInstance,
+): TanstackTableRowActionsType<UniRefund_CRMService_AffiliationTypes_AffiliationTypeDetailDto>[] {
+  const actions: TanstackTableRowActionsType<UniRefund_CRMService_AffiliationTypes_AffiliationTypeDetailDto>[] =
+    [];
+
+  actions.push(
+    {
+      type: "confirmation-dialog",
+      cta: languageData["Merchants.Individual.Create.User"],
+      title: languageData["Merchants.Individual.Create.User"],
+      actionLocation: "row",
+      confirmationText: languageData.Save,
+      cancelText: languageData.Cancel,
+      description: languageData["Merchants.Individual.Create.User.Description"],
+      icon: User2,
+      onConfirm: (row) => {
+        void postAbpUserAccountByIndividualIdApi(row.partyId || "").then(
+          (res) => {
+            handlePostResponse(res, router);
+          },
+        );
+      },
+    },
+    {
+      type: "simple",
+      cta: languageData["Merchants.Individual.SetPassword"],
+      actionLocation: "row",
+      icon: Eye,
+      onClick: (row) => {
+        router.push(`/management/identity/users/${row.abpUserId}/set-password`);
+      },
+    },
+    {
+      type: "confirmation-dialog",
+      cta: languageData["Merchants.Individual.Send.Password.Code.Reset"],
+      title: languageData["Merchants.Individual.Send.Password.Code.Reset"],
+      actionLocation: "row",
+      confirmationText: languageData.Save,
+      cancelText: languageData.Cancel,
+      description:
+        languageData[
+          "Merchants.Individual.Send.Password.Code.Reset.Description"
+        ],
+      icon: Eye,
+      onConfirm: (row) => {
+        void postSendPasswordResetCodeApi({
+          requestBody: {
+            email: row.email || "",
+            appName: "Angular",
+          },
+        }).then((res) => {
+          handlePostResponse(res, router);
+        });
+      },
+    },
+    {
+      type: "custom-dialog",
+      cta: languageData["Merchants.Individual.Lock.User"],
+      title: languageData["Merchants.Individual.Lock.User"],
+      actionLocation: "row",
+      icon: KeyIcon,
+      content: (row) => {
+        return (
+          <SchemaForm
+            onSubmit={(data) => {
+              const formData = data.formData as FormData;
+              void putUsersByIdLockByLockoutEndApi({
+                id: row.abpUserId || "",
+                lockoutEnd: formData.lockoutEnd || "",
+              }).then((res) => {
+                handlePutResponse(res, router);
+              });
+            }}
+            schema={{
+              type: "object",
+              properties: {
+                lockoutEnd: {
+                  type: "string",
+                  format: "date-time",
+                },
+              },
+            }}
+          />
+        );
+      },
+    },
+    {
+      type: "confirmation-dialog",
+      cta: languageData["Merchants.Individual.Unlock.User"],
+      title: languageData["Merchants.Individual.Unlock.User"],
+      actionLocation: "row",
+      confirmationText: languageData.Save,
+      cancelText: languageData.Cancel,
+      description: languageData["Merchants.Individual.Unlock.User.Description"],
+      icon: KeyIcon,
+      onConfirm: (row) => {
+        void putUsersByIdUnlockApi(row.abpUserId || "").then((res) => {
+          handlePostResponse(res, router);
+        });
+      },
+    },
+  );
+  return actions;
+}
 
 export function individualsColumns(
   languageData: CRMServiceServiceResource,
@@ -21,24 +147,6 @@ export function individualsColumns(
     {
       languageData,
       rows: $UniRefund_CRMService_AffiliationTypes_AffiliationTypeDetailDto.properties,
-      faceted: {
-        isAbpUser: {
-          options: [
-            {
-              value: "true",
-              label: "",
-              icon: CheckCircle,
-              iconClassName: "text-green-700",
-            },
-            {
-              value: "false",
-              label: "",
-              icon: XCircle,
-              iconClassName: "text-red-700",
-            },
-          ],
-        },
-      },
       config: {
         locale,
       },
@@ -51,21 +159,29 @@ function individualsTable(
   addAffilationsFormSchema: ZodObjectOrWrapped,
   handleSubmit: (formData: AutoFormValues) => void,
   fieldConfig: FieldConfigType,
+  router: AppRouterInstance,
 ): IndividualsTable {
   const table: IndividualsTable = {
     fillerColumn: "name",
-    columnOrder: ["name"],
     columnVisibility: {
       type: "show",
       columns: [
         "name",
         "codeName",
-        "isAbpUser",
         "email",
         "telephone",
         "entityInformationTypeCode",
+        "abpUserId",
       ],
     },
+    columnOrder: [
+      "name",
+      "codeName",
+      "email",
+      "telephone",
+      "entityInformationTypeCode",
+      "abpUserId",
+    ],
     filters: {
       textFilters: ["name", "email", "telephone"],
       facetedFilters: {
@@ -93,6 +209,7 @@ function individualsTable(
         submitText: languageData.Save,
       },
     ],
+    rowActions: individualsRowActions(languageData, router),
   };
   return table;
 }
--- apps/web/src/app/[lang]/(main)/(unirefund)/parties/[partyName]/[partyId]/_components/individuals-table/table.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/[partyName]/[partyId]/_components/individuals-table/table.tsx
@@ -98,6 +98,7 @@ function Individual({
     affiliationsSchema,
     handleSubmit,
     fieldConfig,
+    router,
   );
 
   return (
