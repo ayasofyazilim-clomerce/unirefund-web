af73a4f01b7862f2533c7646f8bc5af444449f93
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-20 13:46:39 +0300

    feat: add backend functions

--- apps/web/src/actions/unirefund/CrmService/detele-actions.ts
+++ apps/web/src/actions/unirefund/CrmService/detele-actions.ts
@@ -1,4 +1,5 @@
 "use server";
+import type { DeleteApiCrmServiceMerchantsByIdProductGroupsData } from "@ayasofyazilim/saas/CRMService";
 import {
   getCRMServiceClient,
   structuredError,
@@ -12,6 +13,21 @@ export async function deleteAffiliationCodeApi(id: number) {
       await crmClient.affiliationCode.deleteApiCrmServiceAffiliationCodesById({
         id,
       });
+    return structuredResponse(response);
+  } catch (error) {
+    return structuredError(error);
+  }
+}
+
+export async function deleteMerchantsByIdProductGroupsApi(
+  data: DeleteApiCrmServiceMerchantsByIdProductGroupsData,
+) {
+  try {
+    const crmClient = await getCRMServiceClient();
+    const response =
+      await crmClient.merchant.deleteApiCrmServiceMerchantsByIdProductGroups(
+        data,
+      );
     return structuredResponse(response);
   } catch (error) {
     return structuredError(error);
