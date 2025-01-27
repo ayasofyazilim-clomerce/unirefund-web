a268b16f6c83294ca1213fb397a3c5f86f85a2f0
Eren <ahmeterenodaci@gmail.com>
2025-01-27 13:32:42 +0300

    feat(tax-free): enhance API request handling for tax-free entities with improved error management

--- apps/web/src/app/[lang]/(main)/(unirefund)/parties/tax-free/page.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/tax-free/page.tsx
@@ -1,10 +1,10 @@
 "use server";
 
 import type { GetApiCrmServiceTaxFreesData } from "@ayasofyazilim/saas/CRMService";
+import { auth } from "@repo/utils/auth/next-auth";
 import { getTaxFreesApi } from "src/actions/unirefund/CrmService/actions";
 import { getResourceData } from "src/language-data/unirefund/CRMService";
 import { isUnauthorized } from "src/utils/page-policy/page-policy";
-import { isErrorOnRequest } from "src/utils/page-policy/utils";
 import ErrorComponent from "../../../_components/error-component";
 import TaxFreeTable from "./table";
 
@@ -15,6 +15,22 @@ interface SearchParamType {
   name?: string;
 }
 
+async function getApiRequests(filters: GetApiCrmServiceTaxFreesData) {
+  try {
+    const session = await auth();
+    const apiRequests = await Promise.all([getTaxFreesApi(filters, session)]);
+    return {
+      type: "success" as const,
+      data: apiRequests,
+    };
+  } catch (error) {
+    const err = error as { data?: string; message?: string };
+    return {
+      type: "error" as const,
+      message: err.message,
+    };
+  }
+}
 export default async function Page({
   params,
   searchParams,
@@ -28,20 +44,22 @@ export default async function Page({
     lang,
   });
   const { languageData } = await getResourceData(lang);
-  const taxFreeResponse = await getTaxFreesApi({
+
+  const apiRequests = await getApiRequests({
     name: searchParams?.name || "",
     maxResultCount: searchParams?.maxResultCount || 10,
     skipCount: searchParams?.skipCount || 0,
   } as GetApiCrmServiceTaxFreesData);
-
-  if (isErrorOnRequest(taxFreeResponse, lang, false)) {
+  if (apiRequests.type === "error") {
     return (
       <ErrorComponent
         languageData={languageData}
-        message={taxFreeResponse.message}
+        message={apiRequests.message || "Unknown error occurred"}
       />
     );
   }
+
+  const [taxFreeResponse] = apiRequests.data;
 
   return (
     <TaxFreeTable languageData={languageData} response={taxFreeResponse.data} />
