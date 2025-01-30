884df10025e103e885c88a77244c66589644ba95
yusualhashash <yusufalhashash2018@gmail.com>
2025-01-30 08:49:55 +0300

    refactor(taxfree): add create taxfree organization form

---
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/tax-free/new/page.tsx
@@ -0,0 +1,69 @@
+"use server";
+
+import { auth } from "@repo/utils/auth/next-auth";
+import { getTaxOfficesApi } from "src/actions/unirefund/CrmService/actions";
+import ErrorComponent from "src/app/[lang]/(main)/_components/error-component";
+import { getResourceData } from "src/language-data/unirefund/CRMService";
+import { isUnauthorized } from "src/utils/page-policy/page-policy";
+import { getAllCountriesApi } from "../../../../../../../actions/unirefund/LocationService/actions";
+import TaxFreeOrganizationForm from "./_components/form";
+
+async function getApiRequests() {
+  try {
+    const session = await auth();
+    const apiRequests = await Promise.all([
+      getTaxOfficesApi(
+        {
+          maxResultCount: 1000,
+        },
+        session,
+      ),
+      getAllCountriesApi({}, session),
+    ]);
+    return {
+      type: "success" as const,
+      data: apiRequests,
+    };
+  } catch (error) {
+    const err = error as { data?: string; message?: string };
+    return {
+      type: "error" as const,
+      message: err.message,
+    };
+  }
+}
+
+export default async function Page({
+  params,
+}: {
+  params: {
+    lang: string;
+  };
+}) {
+  const { lang } = params;
+  const { languageData } = await getResourceData(lang);
+  await isUnauthorized({
+    requiredPolicies: ["CRMService.TaxFrees.Create"],
+    lang,
+  });
+
+  const apiRequests = await getApiRequests();
+  if (apiRequests.type === "error") {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={apiRequests.message || "Unknown error occurred"}
+      />
+    );
+  }
+
+  const [taxOfficeResponse, countriesResponse] = apiRequests.data;
+
+  return (
+    <TaxFreeOrganizationForm
+      countryList={countriesResponse.data.items || []}
+      languageData={languageData}
+      taxOfficeList={taxOfficeResponse.data.items || []}
+    />
+  );
+}
