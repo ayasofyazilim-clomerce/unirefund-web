e295c5225f7ce12a408b14d8d2964d2f30dd6a0a
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-01-30 11:36:06 +0300

    feat(refund-tables): enhance refund table forms with language data and new components

---
+++ apps/web/src/app/[lang]/(main)/(unirefund)/settings/templates/refund-tables/_components/refund-table-details-field.tsx
@@ -0,0 +1,42 @@
+import type { UniRefund_ContractService_Refunds_RefundTableDetails_RefundTableDetailCreateDto as RefundTableDetailCreateDto } from "@ayasofyazilim/saas/ContractService";
+import { $UniRefund_ContractService_Refunds_RefundTableDetails_RefundTableDetailCreateDto as $RefundTableDetailCreateDto } from "@ayasofyazilim/saas/ContractService";
+import { tanstackTableEditableColumnsByRowData } from "@repo/ayasofyazilim-ui/molecules/tanstack-table/utils";
+import { TableField } from "@repo/ayasofyazilim-ui/organisms/schema-form/fields";
+import { PlusCircle } from "lucide-react";
+
+export function RefundTableDetailsField(
+  data: RefundTableDetailCreateDto[] = [
+    {
+      maxValue: 0,
+      minValue: 0,
+      refundAmount: 0,
+      refundPercent: 0,
+      vatRate: 0,
+    },
+  ],
+) {
+  return TableField<RefundTableDetailCreateDto>({
+    fillerColumn: "isLoyalty",
+    editable: true,
+    data,
+    columns: tanstackTableEditableColumnsByRowData<RefundTableDetailCreateDto>({
+      rows: $RefundTableDetailCreateDto.properties,
+      excludeColumns: ["extraProperties"],
+    }),
+    tableActions: [
+      {
+        type: "create-row",
+        actionLocation: "table",
+        cta: "Add",
+        icon: PlusCircle,
+      },
+    ],
+    rowActions: [
+      {
+        actionLocation: "row",
+        cta: "Delete",
+        type: "delete-row",
+      },
+    ],
+  });
+}
