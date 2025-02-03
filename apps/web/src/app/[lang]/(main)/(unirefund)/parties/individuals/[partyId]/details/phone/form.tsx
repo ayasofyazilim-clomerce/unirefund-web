31b88aa212b2b470760345179ec876e27e641c32
yusualhashash <yusufalhashash2018@gmail.com>
2025-02-03 11:08:05 +0300

    feat: afdd phone edit form

---
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/individuals/[partyId]/details/phone/form.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import type { GetApiCrmServiceIndividualsByIdTelephonesResponse } from "@ayasofyazilim/saas/CRMService";
+import AutoForm, {
+  AutoFormSubmit,
+} from "@repo/ayasofyazilim-ui/organisms/auto-form";
+import { telephoneSchema } from "@repo/ui/utils/table/form-schemas";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { handlePutResponse } from "src/actions/core/api-utils-client";
+import { putIndividualTelephoneApi } from "src/actions/unirefund/CrmService/put-actions";
+import type { TelephoneUpdateDto } from "src/actions/unirefund/CrmService/types";
+import type { CRMServiceServiceResource } from "src/language-data/unirefund/CRMService";
+import { isPhoneValid, splitPhone } from "src/utils/utils-phone";
+
+function TelephoneForm({
+  languageData,
+  partyId,
+  phoneResponse,
+}: {
+  languageData: CRMServiceServiceResource;
+  partyId: string;
+  phoneResponse: GetApiCrmServiceIndividualsByIdTelephonesResponse;
+}) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const phoneValues = phoneResponse[0];
+
+  const telephoneValues = {
+    localNumber:
+      (phoneValues.ituCountryCode || "+90") +
+      (phoneValues.areaCode || "") +
+      (phoneValues.localNumber || ""),
+    primaryFlag: phoneValues.primaryFlag,
+    typeCode: phoneValues.typeCode,
+  };
+
+  function handleSubmit(formData: TelephoneUpdateDto) {
+    startTransition(() => {
+      void putIndividualTelephoneApi({
+        requestBody: formData,
+        id: partyId,
+        telephoneId: phoneValues.id || "",
+      }).then((response) => {
+        handlePutResponse(response, router);
+      });
+    });
+  }
+  return (
+    <AutoForm
+      className="m-4"
+      fieldConfig={{
+        localNumber: {
+          fieldType: "phone",
+          displayName: "Telephone Number",
+          inputProps: {
+            showLabel: true,
+          },
+        },
+      }}
+      formSchema={telephoneSchema}
+      onSubmit={(values) => {
+        const isValid = isPhoneValid(values.localNumber as string);
+        if (!isValid) {
+          return;
+        }
+        const splitedPhoneData = splitPhone(values.localNumber as string);
+        const formData = {
+          ...values,
+          ...splitedPhoneData,
+        } as TelephoneUpdateDto;
+        handleSubmit(formData);
+      }}
+      stickyChildren
+      values={telephoneValues}
+    >
+      <AutoFormSubmit className="float-right mr-6" disabled={isPending}>
+        {languageData["Edit.Save"]}
+      </AutoFormSubmit>
+    </AutoForm>
+  );
+}
+
+export default TelephoneForm;
