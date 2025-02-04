bbeeade9d05094ee32b82af98457d58e3c28b463
yusualhashash <yusufalhashash2018@gmail.com>
2025-02-04 11:19:30 +0300

    feat: add header to all pages

--- apps/web/src/app/[lang]/(main)/(unirefund)/parties/tax-offices/[partyId]/layout.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/tax-offices/[partyId]/layout.tsx
@@ -1,8 +1,30 @@
 "use server";
 
 import { TabLayout } from "@repo/ayasofyazilim-ui/templates/tab-layout";
+import { auth } from "@repo/utils/auth/next-auth";
+import { getTaxOfficeDetailsByIdApi } from "@/actions/unirefund/CrmService/actions";
+import ErrorComponent from "@/app/[lang]/(main)/_components/error-component";
 import { getResourceData } from "src/language-data/unirefund/CRMService";
 import { getBaseLink } from "src/utils";
+
+async function getApiRequests({ partyId }: { partyId: string }) {
+  try {
+    const session = await auth();
+    const apiRequests = await Promise.all([
+      getTaxOfficeDetailsByIdApi(partyId, session),
+    ]);
+    return {
+      type: "success" as const,
+      data: apiRequests,
+    };
+  } catch (error) {
+    const err = error as { data?: string; message?: string };
+    return {
+      type: "error" as const,
+      message: err.message,
+    };
+  }
+}
 
 export default async function Layout({
   children,
@@ -17,26 +39,46 @@ export default async function Layout({
   const { partyId, lang } = params;
   const { languageData } = await getResourceData(lang);
   const baseLink = getBaseLink(`parties/tax-offices/${partyId}/`, lang);
+
+  const apiRequests = await getApiRequests({ partyId });
+  if (apiRequests.type === "error") {
+    return (
+      <ErrorComponent
+        languageData={languageData}
+        message={apiRequests.message || "Unknown error occurred"}
+      />
+    );
+  }
+  const [taxOfficeDetailsResponse] = apiRequests.data;
+
   return (
-    <TabLayout
-      orientation="vertical"
-      tabList={[
-        {
-          label: "Details",
-          href: `${baseLink}details/info`,
-        },
-        {
-          label: languageData["Merchants.SubOrganization"],
-          href: `${baseLink}sub-stores`,
-        },
-        {
-          label: languageData.Affiliations,
-          href: `${baseLink}affiliations`,
-        },
-      ]}
-      variant="simple"
-    >
-      {children}
-    </TabLayout>
+    <>
+      <TabLayout
+        orientation="vertical"
+        tabList={[
+          {
+            label: "Details",
+            href: `${baseLink}details/info`,
+          },
+          {
+            label: languageData["Merchants.SubOrganization"],
+            href: `${baseLink}sub-stores`,
+          },
+          {
+            label: languageData.Affiliations,
+            href: `${baseLink}affiliations`,
+          },
+        ]}
+        variant="simple"
+      >
+        {children}
+      </TabLayout>
+      <div className="hidden" id="page-title">
+        {`${languageData.TaxOffice} (${taxOfficeDetailsResponse.data.entityInformations?.[0]?.organizations?.[0]?.name})`}
+      </div>
+      <div className="hidden" id="page-description">
+        {languageData["TaxOffices.Edit.Description"]}
+      </div>
+    </>
   );
 }
