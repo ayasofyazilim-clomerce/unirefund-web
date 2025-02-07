bc24c65c2f37d003a2db5aec03e2f09bab28e1ff
yusualhashash <yusufalhashash2018@gmail.com>
2025-02-07 16:13:31 +0300

    refactor: add permssions & enhance the tables

--- apps/web/src/app/[lang]/(main)/(unirefund)/parties/merchants/[partyId]/product-groups/product-group-table-data.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/merchants/[partyId]/product-groups/product-group-table-data.tsx
@@ -10,6 +10,8 @@ import type {UniRefund_SettingService_ProductGroups_ProductGroupDto} from "@ayas
 import type {
   TanstackTableColumnLink,
   TanstackTableCreationProps,
+  TanstackTableRowActionsType,
+  TanstackTableTableActionsType,
 } from "@repo/ayasofyazilim-ui/molecules/tanstack-table/types";
 import {tanstackTableCreateColumnsByRowData} from "@repo/ayasofyazilim-ui/molecules/tanstack-table/utils";
 import {SchemaForm} from "@repo/ayasofyazilim-ui/organisms/schema-form";
@@ -30,6 +32,113 @@ import type {Policy} from "src/utils/page-policy/utils";
 type ProductGroupsTable =
   TanstackTableCreationProps<UniRefund_SettingService_ProductGroupMerchants_ProductGroupMerchantRelationDto>;
 
+function productGroupsTableActions(
+  languageData: CRMServiceServiceResource,
+  router: AppRouterInstance,
+  grantedPolicies: Record<Policy, boolean>,
+  productGroupsList: UniRefund_SettingService_ProductGroups_ProductGroupDto[],
+  partyId: string,
+) {
+  const actions: TanstackTableTableActionsType[] = [];
+  if (isActionGranted(["SettingService.ProductGroupMerchants.Add"], grantedPolicies)) {
+    actions.push({
+      type: "custom-dialog",
+      actionLocation: "table",
+      cta: languageData["Merchant.ProductGroup.Add"],
+      title: languageData["Merchant.ProductGroup.Add"],
+      icon: Plus,
+      content: (
+        <SchemaForm<UniRefund_SettingService_ProductGroupMerchants_CreateProductGroupMerchantBaseDto>
+          onSubmit={({formData}) => {
+            if (!formData) return;
+            void postMerchantsByIdProductGroupsApi({
+              id: partyId,
+              requestBody: [formData],
+            }).then((res) => {
+              handlePostResponse(res, router);
+            });
+          }}
+          schema={$UniRefund_SettingService_ProductGroupMerchants_CreateProductGroupMerchantBaseDto}
+          submitText={languageData.Save}
+          uiSchema={createUiSchemaWithResource({
+            schema: $UniRefund_SettingService_ProductGroupMerchants_CreateProductGroupMerchantBaseDto,
+            resources: languageData,
+            name: "Form.Merchant.productGroup",
+            extend: {
+              productGroupId: {
+                "ui:widget": "productGroup",
+              },
+              isDefault: {
+                "ui:widget": "switch",
+              },
+            },
+          })}
+          widgets={{
+            productGroup: CustomComboboxWidget<UniRefund_SettingService_ProductGroups_ProductGroupDto>({
+              languageData,
+              list: productGroupsList,
+              selectIdentifier: "id",
+              selectLabel: "name",
+            }),
+          }}
+        />
+      ),
+    });
+  }
+  return actions;
+}
+
+function productGroupsRowActions(
+  languageData: CRMServiceServiceResource,
+  router: AppRouterInstance,
+  grantedPolicies: Record<Policy, boolean>,
+  partyId: string,
+) {
+  const actions: TanstackTableRowActionsType<UniRefund_SettingService_ProductGroupMerchants_ProductGroupMerchantRelationDto>[] =
+    [];
+  if (isActionGranted(["SettingService.ProductGroupMerchants.Edit"], grantedPolicies)) {
+    actions.push({
+      type: "confirmation-dialog",
+      actionLocation: "row",
+      cta: languageData["Merchant.ProductGroup.Default"],
+      title: languageData["Merchant.ProductGroup.Default"],
+      confirmationText: languageData.Save,
+      cancelText: languageData.Cancel,
+      description: languageData["Merchant.ProductGroup.Default.Description"],
+      icon: Star,
+      onConfirm: (row) => {
+        void postMerchantsByIdProductGroupByProductGroupIdDefaultApi({
+          id: partyId,
+          productGroupId: row.productGroupId,
+        }).then((response) => {
+          handlePostResponse(response, router);
+        });
+      },
+    });
+  }
+  if (isActionGranted(["SettingService.ProductGroupMerchants.Delete"], grantedPolicies)) {
+    actions.push({
+      type: "confirmation-dialog",
+      actionLocation: "row",
+      cta: languageData.Delete,
+      title: languageData["Merchant.ProductGroup.Delete"],
+      confirmationText: languageData.Delete,
+      cancelText: languageData.Cancel,
+      description: languageData["Merchant.ProductGroup.Delete.Description"],
+      icon: Trash,
+      onConfirm: (row) => {
+        void deleteMerchantsByIdProductGroupsApi({
+          id: partyId,
+          requestBody: [row.productGroupId],
+        }).then((response) => {
+          handleDeleteResponse(response, router);
+        });
+      },
+    });
+  }
+  return actions;
+}
+
 export function productGroupsColumns(
   languageData: CRMServiceServiceResource,
   grantedPolicies: Record<Policy, boolean>,
@@ -45,6 +154,10 @@ export function productGroupsColumns(
     links.productGroupName = {
       prefix: "/settings/product/product-groups",
       targetAccessorKey: "productGroupId",
+    };
+    links.vatRate = {
+      prefix: "/settings/product/vats",
+      targetAccessorKey: "vatId",
     };
   }
 
@@ -63,16 +176,24 @@ export function productGroupsColumns(
         isActive: {
           options: [
             {
+              label: "Yes",
+              when: (value) => {
+                return Boolean(value);
+              },
               value: "true",
-              label: "",
               icon: CheckCircle,
               iconClassName: "text-green-700",
+              hideColumnValue: true,
             },
             {
+              label: "No",
+              when: (value) => {
+                return !value;
+              },
               value: "false",
-              label: "",
               icon: XCircle,
               iconClassName: "text-red-700",
+              hideColumnValue: true,
             },
           ],
         },
@@ -102,98 +223,18 @@ function productGroupsTable(
   languageData: CRMServiceServiceResource,
   router: AppRouterInstance,
   productGroupsList: UniRefund_SettingService_ProductGroups_ProductGroupDto[],
-  partId: string,
+  partyId: string,
+  grantedPolicies: Record<Policy, boolean>,
 ): ProductGroupsTable {
   const table: ProductGroupsTable = {
     fillerColumn: "productGroupName",
     columnVisibility: {
       type: "hide",
-      columns: ["isAssign", "isDefault", "productGroupId"],
+      columns: ["isAssign", "isDefault", "productGroupId", "vatId"],
     },
-    columnOrder: ["productGroupName", "isActive"],
-    tableActions: [
-      {
-        type: "custom-dialog",
-        actionLocation: "table",
-        cta: languageData["Merchant.ProductGroup.Add"],
-        title: languageData["Merchant.ProductGroup.Add"],
-        icon: Plus,
-        content: (
-          <SchemaForm<UniRefund_SettingService_ProductGroupMerchants_CreateProductGroupMerchantBaseDto>
-            onSubmit={({formData}) => {
-              if (!formData) return;
-              void postMerchantsByIdProductGroupsApi({
-                id: partId,
-                requestBody: [formData],
-              }).then((res) => {
-                handlePostResponse(res, router);
-              });
-            }}
-            schema={$UniRefund_SettingService_ProductGroupMerchants_CreateProductGroupMerchantBaseDto}
-            submitText={languageData.Save}
-            uiSchema={createUiSchemaWithResource({
-              schema: $UniRefund_SettingService_ProductGroupMerchants_CreateProductGroupMerchantBaseDto,
-              resources: languageData,
-              name: "Form.Merchant.productGroup",
-              extend: {
-                productGroupId: {
-                  "ui:widget": "productGroup",
-                },
-                isDefault: {
-                  "ui:widget": "switch",
-                },
-              },
-            })}
-            widgets={{
-              productGroup: CustomComboboxWidget<UniRefund_SettingService_ProductGroups_ProductGroupDto>({
-                languageData,
-                list: productGroupsList,
-                selectIdentifier: "id",
-                selectLabel: "name",
-              }),
-            }}
-          />
-        ),
-      },
-    ],
-    rowActions: [
-      {
-        type: "confirmation-dialog",
-        actionLocation: "row",
-        cta: languageData.Delete,
-        title: languageData["Merchant.ProductGroup.Delete"],
-        confirmationText: languageData.Delete,
-        cancelText: languageData.Cancel,
-        description: languageData["Merchant.ProductGroup.Delete.Description"],
-        icon: Trash,
-        onConfirm: (row) => {
-          void deleteMerchantsByIdProductGroupsApi({
-            id: partId,
-            requestBody: [row.productGroupId],
-          }).then((response) => {
-            handleDeleteResponse(response, router);
-          });
-        },
-      },
-      {
-        type: "confirmation-dialog",
-        actionLocation: "row",
-        cta: languageData["Merchant.ProductGroup.Default"],
-        title: languageData["Merchant.ProductGroup.Default"],
-        confirmationText: languageData.Save,
-        cancelText: languageData.Cancel,
-        description: languageData["Merchant.ProductGroup.Default.Description"],
-        icon: Star,
-        onConfirm: (row) => {
-          void postMerchantsByIdProductGroupByProductGroupIdDefaultApi({
-            id: partId,
-            productGroupId: row.productGroupId,
-          }).then((response) => {
-            handlePostResponse(response, router);
-          });
-        },
-      },
-    ],
+    columnOrder: ["productGroupName", "vatRate", "isActive"],
+    tableActions: productGroupsTableActions(languageData, router, grantedPolicies, productGroupsList, partyId),
+    rowActions: productGroupsRowActions(languageData, router, grantedPolicies, partyId),
   };
   return table;
 }
