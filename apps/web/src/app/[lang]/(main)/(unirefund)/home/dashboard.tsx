fe965ac1a9de6e61319cee0d21e6e61c7df06f46
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-04-11 09:22:52 +0300

    feat: add support for Superset URL and Dashboard ID in environment and dashboard components

--- apps/web/src/app/[lang]/(main)/(unirefund)/home/dashboard.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/home/dashboard.tsx
@@ -2,26 +2,21 @@
 import {embedDashboard} from "@superset-ui/embedded-sdk";
 import {useEffect} from "react";
 
-const supersetUrl = "http://192.168.1.107:8089";
-const dashboardId = "58a2e4b8-a0da-4e73-9f98-d8946fc908f7"; // replace with your dashboard id
-
-export default function Dashboard() {
+export default function Dashboard({supersetUrl, dashboardId}: {supersetUrl: string; dashboardId: string}) {
   useEffect(() => {
     const mountPoint = document.getElementById("dashboard-embed");
     if (mountPoint) {
       void embedDashboard({
-        id: dashboardId, // Use the id obtained from enabling embedding dashboard option
+        id: dashboardId,
         supersetDomain: supersetUrl,
-        mountPoint, // html element in which iframe will be mounted to show the dashboard
-        fetchGuestToken: () => getToken(),
+        mountPoint,
+        fetchGuestToken: () => getToken(dashboardId),
         dashboardUiConfig: {
-          // hideTitle: true,
-          // hideTab:true
           filters: {
             expanded: true,
           },
           urlParams: {
-            standalone: 3, // here you can add the url_params and there values
+            standalone: 3,
           },
         },
       });
@@ -30,8 +25,7 @@ export default function Dashboard() {
   return <div className="mb-4 size-full [&>iframe]:size-full" id="dashboard-embed" />;
 }
 
-async function getToken() {
-  // This uses admin creds to fetch the token
+async function getToken(dashboardId: string) {
   const x = (await fetch(`/api/token/?dashboardId=${dashboardId}`)).text();
   return x;
 }
