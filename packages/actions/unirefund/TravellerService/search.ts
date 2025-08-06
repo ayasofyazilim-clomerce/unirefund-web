9feb1b0ffd7960a160da6f431a5eb00c80a126ea
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-08-06 09:15:49 +0300

    fix:lint

--- packages/actions/unirefund/TravellerService/search.ts
+++ packages/actions/unirefund/TravellerService/search.ts
@@ -5,7 +5,7 @@ import {getTravellersApi} from "./actions";
 export async function searchTravellers(search: string) {
   try {
     const res = await getTravellersApi({fullName: search});
-    return res.data.items?.map((i) => ({id: i.id, name: i.fullName})) || [];
+    return res.data.items?.map((i) => ({id: i.id ?? "", name: i.fullName})) || [];
   } catch (error) {
     return [];
   }
