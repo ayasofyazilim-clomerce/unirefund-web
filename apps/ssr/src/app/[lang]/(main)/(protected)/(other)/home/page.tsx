6dd16e626d9452c50db7bb0f3d5c2464cb1e9881
muhammed-gumus <mr.silver.mg@gmail.com>
2025-09-11 11:45:39 +0300

    feat: add tags response to HomePageClient and integrate TagsTable component

--- apps/ssr/src/app/[lang]/(main)/(protected)/(other)/home/page.tsx
+++ apps/ssr/src/app/[lang]/(main)/(protected)/(other)/home/page.tsx
@@ -1,3 +1,4 @@
+import {getTagsApi} from "@repo/actions/unirefund/TagService/actions";
 import {getResourceData} from "src/language-data/unirefund/SSRService";
 import HomePageClient from "./client";
 
@@ -11,6 +12,8 @@ export default async function Home({
   const {lang} = params;
   const {languageData} = await getResourceData(lang);
 
+  const tagsResponse = await getTagsApi();
+
   // Pass the language data to the client component
-  return <HomePageClient languageData={languageData} />;
+  return <HomePageClient languageData={languageData} tagsResponse={tagsResponse} />;
 }
