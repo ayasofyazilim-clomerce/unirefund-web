06b8e3841307413a04e771155bd2874787db8ead
muhammed-gumus <mr.silver.mg@gmail.com>
2025-09-11 13:52:38 +0300

    feat: implement API request handling and error management in Home component

--- apps/ssr/src/app/[lang]/(main)/(protected)/(other)/home/page.tsx
+++ apps/ssr/src/app/[lang]/(main)/(protected)/(other)/home/page.tsx
@@ -1,6 +1,25 @@
 import {getTagsApi} from "@repo/actions/unirefund/TagService/actions";
 import {getResourceData} from "src/language-data/unirefund/SSRService";
 import HomePageClient from "./client";
+import type {GetApiTagServiceTagData} from "@repo/saas/TagService";
+import ErrorComponent from "@repo/ui/components/error-component";
+import {structuredError} from "@repo/utils/api";
+import {auth} from "@repo/utils/auth/next-auth";
+import {isRedirectError} from "next/dist/client/components/redirect";
+
+async function getApiRequests(filters: GetApiTagServiceTagData) {
+  try {
+    const session = await auth();
+    const requiredRequests = await Promise.all([getTagsApi(filters, session)]);
+    const optionalRequests = await Promise.allSettled([]);
+    return {requiredRequests, optionalRequests};
+  } catch (error) {
+    if (!isRedirectError(error)) {
+      return structuredError(error);
+    }
+    throw error;
+  }
+}
 
 export default async function Home({
   params,
@@ -12,8 +31,13 @@ export default async function Home({
   const {lang} = params;
   const {languageData} = await getResourceData(lang);
 
-  const tagsResponse = await getTagsApi();
+  const apiRequests = await getApiRequests({} as GetApiTagServiceTagData);
 
-  // Pass the language data to the client component
+  if ("message" in apiRequests) {
+    return <ErrorComponent languageData={languageData} message={apiRequests.message} />;
+  }
+
+  const [tagsResponse] = apiRequests.requiredRequests;
+
   return <HomePageClient languageData={languageData} tagsResponse={tagsResponse} />;
 }
