c2a7a8eaf6509be1cdcdac7bb339aabfb9699fd9
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-09-16 14:59:57 +0300

    chore:extend user token type for affiliation

--- packages/ui/src/unirefund/party-switcher/index.tsx
+++ packages/ui/src/unirefund/party-switcher/index.tsx
@@ -1,57 +1,94 @@
+"use client"
 import { Label } from "@repo/ayasofyazilim-ui/atoms/label"
+import { CheckboxTree } from "@repo/ayasofyazilim-ui/atoms/checkbox-tree"
+import { Checkbox } from "@repo/ayasofyazilim-ui/atoms/checkbox"
+import { Popover, PopoverAnchor, PopoverContent, PopoverTrigger } from "@repo/ayasofyazilim-ui/atoms/popover"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@repo/ayasofyazilim-ui/atoms/select"
-import { useId } from "react"
+import { Fragment, useId } from "react"
 import { cn } from "../../utils"
+import { UniRefund_CRMService_UserAffiliations_UserAffiliationDto as Affiliation } from "@repo/saas/CRMService"
+import { Check, Minus } from "lucide-react"
 
 
-const Square = ({
+
+
+export function PartySwitcher({
+    activeIds,
+    affiliations,
+}: {
+    activeIds: string[]
+    affiliations: Affiliation[]
+}) {
+    const id = useId()
+    const activeId = activeIds[0];
+    const active = affiliations.find((aff) => aff.partyId === activeId);
+    const parentAffiliations = affiliations.filter((aff) => aff.parentId === null);
+
+    return (
+        <div className="*:not-first:mt-2">
+            <Select defaultValue={activeId}>
+                <SelectTrigger
+                    id={id}
+                    className="ps-2 [&>span]:flex [&>span]:items-center [&>span]:gap-2 [&>span_[data-square]]:shrink-0 min-w-52 shadow-none"
+                >
+                    <SelectValue placeholder="Select framework" />
+                </SelectTrigger>
+                <SelectContent className="[&_*[role=option]]:ps-2 [&_*[role=option]]:pe-8 [&_*[role=option]>span]:start-auto [&_*[role=option]>span]:end-2 [&_*[role=option]>span]:flex [&_*[role=option]>span]:items-center [&_*[role=option]>span]:gap-2">
+                    {parentAffiliations.map((affiliation) => {
+                        const childrenAffiliations = affiliations.filter((aff) => aff.parentId === affiliation.partyId);
+                        return (
+                            <SelectGroup>
+                                <SelectItem value={affiliation.partyId || ""} className="">
+                                    <span className="flex items-center gap-2">
+                                        {activeId == affiliation.partyId && <Square isActive />}
+                                        {affiliation.partyName}
+                                    </span>
+                                </SelectItem>
+                                {childrenAffiliations.map((childAffiliation) => {
+                                    const isActive = activeIds.includes(childAffiliation.partyId || "");
+                                    return (
+                                        <SelectItem value={childAffiliation.partyId || ""} className="!pl-8">
+                                            <span className="flex items-center gap-2">
+                                                {isActive && <Square isActive={isActive} />}
+                                                {childAffiliation.partyName}
+                                            </span>
+                                        </SelectItem>
+                                    );
+                                })}
+                            </SelectGroup>
+                        )
+                    })}
+                </SelectContent>
+            </Select>
+        </div>
+    )
+}
+
+function Square({ isActive }: { isActive: boolean }) {
+    if (isActive) return <SquareCore className="bg-green-400/20 text-green-500">
+        <Check className="w-4" />
+    </SquareCore>
+    return <SquareCore className="bg-muted text-muted-foreground">
+        <Minus className="w-4" />
+    </SquareCore>
+}
+function SquareCore({
     className,
     children,
 }: {
     className?: string
     children: React.ReactNode
-}) => (
-    <span
-        data-square
-        className={cn(
-            "bg-muted text-muted-foreground flex size-5 items-center justify-center rounded text-xs font-medium",
-            className
-        )}
-        aria-hidden="true"
-    >
-        {children}
-    </span>
-)
-
-export function PartySwitcher() {
-    const id = useId()
+}) {
     return (
-        <div className="*:not-first:mt-2">
-            <Select defaultValue="1">
-                <SelectTrigger
-                    id={id}
-                    className="ps-2 [&>span]:flex [&>span]:items-center [&>span]:gap-2 [&>span_[data-square]]:shrink-0"
-                >
-                    <SelectValue placeholder="Select framework" />
-                </SelectTrigger>
-                <SelectContent className="[&_*[role=option]]:ps-2 [&_*[role=option]]:pe-8 [&_*[role=option]>span]:start-auto [&_*[role=option]>span]:end-2 [&_*[role=option]>span]:flex [&_*[role=option]>span]:items-center [&_*[role=option]>span]:gap-2">
-                    <SelectGroup>
-                        <SelectLabel className="ps-2">Impersonate user</SelectLabel>
-                        <SelectItem value="1">
-                            <Square className="bg-indigo-400/20 text-indigo-500">F</Square>
-                            <span className="truncate">Frank Morris</span>
-                        </SelectItem>
-                        <SelectItem value="2">
-                            <Square className="bg-purple-400/20 text-purple-500">X</Square>
-                            <span className="truncate">Xavier Guerra</span>
-                        </SelectItem>
-                        <SelectItem value="3">
-                            <Square className="bg-rose-400/20 text-rose-500">A</Square>
-                            <span className="truncate">Anne Kelley</span>
-                        </SelectItem>
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
-        </div>
+        <span
+            data-square
+            className={cn(
+                "bg-muted text-muted-foreground flex size-5 items-center justify-center rounded text-xs font-medium",
+                className
+            )}
+            aria-hidden="true"
+        >
+            {children}
+        </span>
     )
 }
