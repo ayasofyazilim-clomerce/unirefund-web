a123789396d04c9a74fc6398bbe0b70404bb5b8c
Eren <ahmeterenodaci@gmail.com>
2025-09-26 16:57:29 +0300

    refactor: update productGroupsTableActions to close dialog on successful post

--- apps/web/src/app/[lang]/(main)/(unirefund)/parties/merchants/[partyId]/product-groups/product-group-table-data.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/parties/merchants/[partyId]/product-groups/product-group-table-data.tsx
@@ -51,7 +51,7 @@ function productGroupsTableActions(
       cta: languageData["Form.Merchant.productGroup.create"],
       title: languageData["Form.Merchant.productGroup.create"],
       icon: Plus,
-      content: (
+      content: (closeDialog) => (
         <SchemaForm<UniRefund_SettingService_ProductGroupMerchants_CreateProductGroupMerchantBaseDto>
           onSubmit={({formData}) => {
             if (!formData) return;
@@ -60,6 +60,10 @@ function productGroupsTableActions(
               requestBody: [formData],
             }).then((res) => {
               handlePostResponse(res, router);
+              // Close dialog if post is successful
+              if (res.type === "success") {
+                closeDialog?.();
+              }
             });
           }}
           schema={$UniRefund_SettingService_ProductGroupMerchants_CreateProductGroupMerchantBaseDto}
