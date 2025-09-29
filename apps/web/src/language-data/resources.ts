6fe798751ede4b93f42eb02a83a8e13940195be6
Eren <ahmeterenodaci@gmail.com>
2025-09-29 10:38:56 +0300

    feat: Add loading components for various parties and settings
    - Introduced loading spinner components for individual, merchant, refund point, tax-free, and tax office parties.
    - Created loading components for contract settings, product groups, and rebate tables.
    - Implemented a centralized loading spinner component for consistent loading UI across the application.
    - Updated language resources to reflect changes in terminology regarding external identifiers.

--- apps/web/src/language-data/resources.ts
+++ apps/web/src/language-data/resources.ts
@@ -1772,7 +1772,7 @@ export type CRMServiceResources = {
   "UniRefund.CRMService:011016": "No merchant found either headquarter with VAT Number: '{vATNumber}' or sub merchant with customer number: '{customerNumber}'!";
   "UniRefund.CRMService:011017": "There is not a headquarter merchant related to given merchant. So, it is not possible to get product groups of non-existent merchant!";
   "UniRefund.CRMService:011018": "A merchant without a filled ParentId cannot be of type headquarters";
-  "UniRefund.CRMService:011019": "ExternalStoreIdentifier cannot be empty";
+  "UniRefund.CRMService:011019": "ExternalIdentifier cannot be empty";
   "UniRefund.CRMService:012001": "No tax office with this id was found: {taxOfficeId}";
   "UniRefund.CRMService:012002": "Tax Office type cannot be changed. Because there are dependent child merchants.";
   "UniRefund.CRMService:013001": "There is no entity AffiliationCode with id = {id}!";
