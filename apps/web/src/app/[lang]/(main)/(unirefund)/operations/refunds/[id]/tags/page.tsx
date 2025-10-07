138568e24433dc8da1b10638a207fc93f9e9effb
eren <eren.odaci@ayasofyazilim.com>
2025-10-07 09:56:43 +0300

    refactor: simplify Page component by removing unused parameters and optimizing imports

--- apps/web/src/app/[lang]/(main)/(unirefund)/operations/refunds/[id]/tags/page.tsx
+++ apps/web/src/app/[lang]/(main)/(unirefund)/operations/refunds/[id]/tags/page.tsx
@@ -1,23 +1,14 @@
-import {isErrorOnRequest} from "@repo/utils/api";
-import ErrorComponent from "@repo/ui/components/error-component";
 import {getTagsApi} from "@repo/actions/unirefund/TagService/actions";
+import ErrorComponent from "@repo/ui/components/error-component";
+import {isErrorOnRequest} from "@repo/utils/api";
 import {getResourceData} from "@/language-data/unirefund/TagService";
-import type {TagsSearchParamType} from "../../../tax-free-tags/_components/utils";
-import {initParams} from "../../../tax-free-tags/_components/utils";
 import TaxFreeTagsTable from "./_components/table";
 
-export default async function Page({
-  params,
-  searchParams,
-}: {
-  params: {lang: string};
-  searchParams: TagsSearchParamType;
-}) {
-  const {lang} = params;
+export default async function Page({params}: {params: {id: string; lang: string}}) {
+  const {lang, id} = params;
   const {languageData} = await getResourceData(lang);
-  const tagData = initParams(searchParams);
 
-  const tagsResponse = await getTagsApi(tagData);
+  const tagsResponse = await getTagsApi({refundId: id});
   if (isErrorOnRequest(tagsResponse, lang, false)) {
     return <ErrorComponent languageData={languageData} message={tagsResponse.message} />;
   }
